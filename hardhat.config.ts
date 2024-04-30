77ddc2451e6858014a1ec3837a9824c38b3f3d71
Recrypto7 <helder.salvador.blockchain@gmail.com>
2024-04-30 16:36:30 +0200

    :wrench: Added new networks
    - Add your own keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,17 @@ const config: HardhatUserConfig = {
     evmSidechain: {
       url: "https://rpc-evm-sidechain.xrpl.org",
       accounts: ["29fee737f77f3f5faee3e18202f14c2be33349528e9023c805656c8528b244d4"]
-    }
+    },
+    networks: {
+      zkEVM: {
+      url: `https://rpc.public.zkevm-test.net`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+    sepolia: {
+      url: "YOUR_SEPOLIA_RPC_URL", // Replace this with your Sepolia RPC URL
+      chainId: YOUR_CHAIN_ID, // Replace this with the chain ID of the Sepolia network
+      accounts: ["YOUR_PRIVATE_KEY"], // Specify the private key of your account for deploying contracts
+    },
   }
 };
 
